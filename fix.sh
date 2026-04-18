72899fdc377c868bf9f2d0c85e8f61739ec6cbfb
gniewomir <gniewomir@wintermute.home>
2026-04-18 18:34:48 +0200

    update 2026-04-18 18:34

--- fix.sh
+++ fix.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 ROOT="$(cd "$(dirname "$0")" && pwd)"
+
+PATH="$PWD/.scripts/.venv/bin:$PATH"
+
 # shellcheck source=.scripts/ensure_python.sh
 . "$ROOT/.scripts/ensure_python.sh"
 
